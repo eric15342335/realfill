7c7d37f8f2692efe5c98bb998d105735ce164922
Chan Hin Chun Jensen <20050910j@gmail.com>
2025-04-27 01:15:38 +0800

    Update Dream_slim.py

--- Dream_slim.py
+++ Dream_slim.py
@@ -3,26 +3,57 @@ from PIL import Image
 import cv2 
 import numpy as np 
 import torch
+import os
 
 device = device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 model, preprocess = dreamsim(pretrained=True, device=device)
 
-def dream(img1, img2):
-	distance = model(img1, img2)
+def dream(img1, img2, mask):
+	mask[mask == 0] = 254
+	mask[mask == 255] = 0
+	mask[mask == 254] = 255
+	distance = model(img1*mask, img2*mask)
 	return distance
 
-def main(m): 
-	original = preprocess(Image.open(rf"C:\Users\Jensen\Documents\APAI3010\Project\realfill_data_release_full\RealBench\{m}\target\gt.png")).to(device)
-	value = 0
-	for i in range(16):
-		compressed= preprocess(Image.open(rf"\Users\Jensen\Documents\APAI3010\Project\realfill_data_release_full\RealBench\{m}\results\{i}.png")).to(device)
-		value += dream(original, compressed) 
-	print(f"Dreamsim value is {value/16} dB") 
-	return value/16
+def main(m):
+	try:
+		original  = preprocess(Image.open(rf"C:\Users\Jensen\Documents\APAI3010\Project\realfill_data_release_full\RealBench-gen\{m}\target\gt.png")).to(device)
+		mask = preprocess(Image.open(rf"C:\Users\Jensen\Documents\APAI3010\Project\realfill_data_release_full\RealBench-gen\{m}\target\mask.png")).to(device)
+		if original is None or mask is None:
+			print (f"Warning: Could not read ground truth or mask for folder {m}. Skipping.")
+			return 0
+	except FileNotFoundError:
+		print(f"Warning: Ground truth not found or mask not found for folder {m}. Skipping.")
+		return 0
 	
+	value = 0
+	count = 0
+	for i in range(16):
+		try:
+			result = preprocess(Image.open(rf"\Users\Jensen\Documents\APAI3010\Project\realfill_data_release_full\RealBench-gen\{m}\results-gen\{i}.png")).to(device)
+			if result is None:
+				print (f"Warning: Could not read ground truth for folder {m}. Skipping.")
+		except  FileNotFoundError:
+			print(f"Warning: Ground truth not found for folder {m}. Skipping.")
+		value += dream(original, result, mask)
+		count += 1
+	if count > 0:
+		print(f"Dreamsim value for folder{m} is {value/count}")
+		return value/count
+	else:
+		print(f"No valid generated images found for folder {m}.")
+		return 0
 
-m = 22
-ans = 0
-for m in range(m):
-	ans += main(m)
-print(f"Average Dreamsim value is {ans/(22)} dB")
+total = 0
+m_limit = 30
+validfolders = 0
+for m in range(m_limit):
+	avg_dream = main(m)
+	if avg_dream != 0:
+		total += avg_dream
+		validfolders += 1
+if validfolders > 0:
+    overall_avg_lpips = total / validfolders
+    print(f"Average LPIPS value (filled regions) across {validfolders} folders is {overall_avg_lpips}")
+else:
+    print("No valid folders found to calculate the average LPIPS.")
